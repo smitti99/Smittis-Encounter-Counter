902e7ff19fdcb13d48310f6a6d738f9edc9ee239
smitti <marius-schmidt11@gmx.de>
2025-05-09 02:00:42 +0200

    use more absolute path

--- tests/test_en_pkmn_recognition.py
+++ tests/test_en_pkmn_recognition.py
@@ -1,13 +1,15 @@
+import os
+
 import cv2
 
 from src import scan_screen
 from src.Enums import EncounterType
 from src.poke_tree import PokeTree
 
-
+base_path = os.path.dirname(os.path.abspath(__file__))
 def test_single_encounter_extraction():
     scan_screen.poke_tree = PokeTree('en')
-    img = cv2.imread("screenshots/en/Ponyta-SINGLE.jpg")
+    img = cv2.imread(os.path.join(base_path,"screenshots/en/Ponyta-SINGLE.jpg"))
     encounter = scan_screen.extract_encounter_from_picture(img)
 
     assert encounter[0] == "Ponyta"
@@ -15,7 +17,7 @@ def test_single_encounter_extraction():
 
 def test_horde_encounter_extraction():
     scan_screen.poke_tree = PokeTree('en')
-    img = cv2.imread("screenshots/en/Tentacruel_HORDE.jpg")
+    img = cv2.imread(os.path.join(base_path,"screenshots/en/Tentacruel_HORDE.jpg"))
     encounter = scan_screen.extract_encounter_from_picture(img)
 
     assert encounter[0] == "Tentacruel"
